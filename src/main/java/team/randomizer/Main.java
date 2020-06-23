1412502c3b928c5defcab58e70756cf8c381a807
Alex Keeler <akeeler@MacBook-Pro.local>
2020-06-23 00:02:35 -0400

    Turns out uploading your bot token to the internet is a no-no

--- src/main/java/team/randomizer/Main.java
+++ src/main/java/team/randomizer/Main.java
@@ -1,15 +1,22 @@
 package team.randomizer;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import team.randomizer.listener.RandomizeListener;
 
 public class Main {
-	
-	public static final String BOT_TOKEN = "NzE3OTY1OTIxMjY4MDA3MDI0.XvFwog.Wm7TaARi70Dqy7atBAo78vI4DOQ";
-	
+		
 	public static void main(String[] arguments) throws Exception {
-	    JDA api = JDABuilder.createDefault(BOT_TOKEN).build();
+		
+		String configPath = Thread.currentThread().getContextClassLoader().getResource("config.properties").getPath();
+		Properties config = new Properties();
+		config.load(new FileInputStream(configPath));
+		
+		String botToken = config.getProperty("bot.token");
+	    JDA api = JDABuilder.createDefault(botToken).build();
 	    
 	    // Setup listeners
 	    api.addEventListener(new RandomizeListener());
